f74dc3e4f78c42d78dca91f5edfb393969ce0405
akhil-17 <dakinedi+1@gmail.com>
2025-03-26 21:34:36 +1100

    Initial Commit

---
+++ PokerHands/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokerHands
+//
+//  Created by Akhil Dakinedi on 3/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
